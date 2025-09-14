5064b69f32fcb042ea9fbe8e2556094a564be963
Sunil Meghwal <sunillmeghwal@gmail.com>
2025-09-14 22:27:17 +0530

    Client-review components added

--- components/Home/ClientReview/ClientReview.tsx
+++ components/Home/ClientReview/ClientReview.tsx
@@ -1,4 +1,26 @@
+"use client";
 import React from "react";
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
+import ClientReviewCard from "./ClientReviewCard";
+
+const responsive = {
+  desktop: {
+    breakpoint: { max: 3000, min: 1324 },
+    items: 3,
+    slidesToSlide: 1, // optional, default to 1.
+  },
+  tablet: {
+    breakpoint: { max: 1324, min: 764 },
+    items: 2,
+    slidesToSlide: 1, // optional, default to 1.
+  },
+  mobile: {
+    breakpoint: { max: 764, min: 0 },
+    items: 1,
+    slidesToSlide: 1, // optional, default to 1.
+  },
+};
 
 const ClientReview = () => {
   return (
@@ -7,8 +29,58 @@ const ClientReview = () => {
         Kind words from satisfied
         <br /> <span className="text-cyan-200"> clients</span>
       </h1>
+      // npm i react-multi-carousel
       <div className="mt-16 w-[70%] mx-auto">
-        
+        <Carousel
+          showDots={false}
+          responsive={responsive}
+          infinite={true}
+          autoPlay={true}
+          autoPlaySpeed={4000}
+        >
+          <ClientReviewCard
+            image="/images/c1.png"
+            name="Janny Doe"
+            role="CEO , Landscape"
+            description='" Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam ad,
+        earum modi sed suscipit praesentium accusantium assumenda odio dolor
+        soluta "'
+          />
+
+          <ClientReviewCard
+            image="/images/c2.png"
+            name="John Doe"
+            role="UI UX Designer"
+            description='" Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam ad,
+        earum modi sed suscipit praesentium accusantium assumenda odio dolor
+        soluta "'
+          />
+          <ClientReviewCard
+            image="/images/c3.png"
+            name="Jassica Doe"
+            role="CEO , Tech Company"
+            description='" Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam ad,
+        earum modi sed suscipit praesentium accusantium assumenda odio dolor
+        soluta "'
+          />
+          <ClientReviewCard
+            image="/images/c4.png"
+            name="Jara Doe"
+            role="Web Developer"
+            description='" Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam ad,
+        earum modi sed suscipit praesentium accusantium assumenda odio dolor
+        soluta "'
+          />
+          <ClientReviewCard
+            image="/images/c5.png"
+            name="Jora Doe"
+            role="CEO , Design Studio"
+            description='" Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam ad,
+        earum modi sed suscipit praesentium accusantium assumenda odio dolor
+        soluta "'
+          />
+        </Carousel>
+        ;
       </div>
     </div>
   );
---
+++ components/Home/ClientReview/ClientReviewCard.tsx
@@ -0,0 +1,29 @@
+import Image from "next/image";
+import React from "react";
+type Props = {
+  name: string;
+  image: string;
+  role: string;
+  description: string;
+};
+
+const ClientReviewCard = ({ image, name, role, description }): Props => {
+  return (
+    <div className="m-2">
+      <Image
+        src={image}
+        alt="client"
+        width={60}
+        height={60}
+        className="rounded-full"
+      />
+      <p className="mt-6 text-basee text-gray-200 font-meedium">
+        {description}
+      </p>
+      <h1 className="mt-6 text-xl font-bold text-cyan-200">{name}</h1>
+      <p className="mt-1 text-white">{role}</p>
+    </div>
+  );
+};
+
+export default ClientReviewCard;
